398b64021c84f18280d41cb99661c63bfc82904b
Dan Munckton <munckfish@gmail.com>
2020-10-31 09:56:55 +0000

    Load a basic word list

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+use std::error::Error;
+use std::io;
+use std::process;
+
+use serde::Deserialize;
+
+// a corresponding field in the CSV data's header record.
+#[derive(Debug, Deserialize)]
+struct Word {
+    word: String,
+    english: String,
+    gender: Option<String>,
+}
+
+fn example() -> Result<(), Box<dyn Error>> {
+    let mut rdr = csv::Reader::from_reader(io::stdin());
+    for result in rdr.deserialize() {
+        // Notice that we need to provide a type hint for automatic
+        // deserialization.
+        let record: Word = result?;
+        println!("{:?}", record);
+    }
+    Ok(())
+}
+
 fn main() {
-    println!("Hello, world!");
+    if let Err(err) = example() {
+        println!("error running example: {}", err);
+        process::exit(1);
+    }
 }
